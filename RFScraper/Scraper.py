a5617d933ee9bddec88b3a60e8e04b1ed113d9b1
Michael Ho <curiousprgrmr@gmail.com>
2013-09-29 03:53:01 -0400

    Scraper now returning PostList object properly

--- RFScraper/Scraper.py
+++ RFScraper/Scraper.py
@@ -7,21 +7,25 @@ import unicodedata
 from Post import Post
 from PostList import PostList
  
-urlList=[]
-urlList.append("http://forums.redflagdeals.com/hot-deals-f9/")
+def scrape():
+	urlList=[]
+	urlList.append("http://forums.redflagdeals.com/hot-deals-f9/")
 
-for x in range(9):
-	urlList.append("http://forums.redflagdeals.com/hot-deals-f9/%d/" % (x+2))
+	for x in range(9):
+		urlList.append("http://forums.redflagdeals.com/hot-deals-f9/%d/" % (x+2))
 
-postList = PostList()
+	postList = PostList()
 
-for x in range(len(urlList)):
-	content = urllib2.urlopen(urlList[x]).read()
-	soup = BeautifulSoup(content)
+	for x in range(len(urlList)):
+		content = urllib2.urlopen(urlList[x]).read()
+		soup = BeautifulSoup(content)
 
-	for link in soup.findAll(True, {'class': re.compile(r'\btitle\b')}):
-	    url = "http://forums.redflagdeals.com" + link.get('href')
-	    title = link.get_text()
-	    title = title.replace(u'\xae', u' ')
-	    post = Post(title, url)
-	    postList.addPost(post)
+		for link in soup.findAll(True, {'class': re.compile(r'\btitle\b')}):
+		    url = "http://forums.redflagdeals.com" + link.get('href')
+		    title = link.get_text()
+		    title = title.replace(u'\xae', u' ')
+		    title = title.replace(u'\xa2', u' ')
+		    title = title.encode('ascii', 'ignore')
+		    post = Post(title, url)
+		    postList.addPost(post)
+	return postList

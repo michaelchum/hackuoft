66f662228fcae9b4a22b3118fd9086d65f5043a3
Michael Ho <curiousprgrmr@gmail.com>
2013-09-29 05:52:28 -0400

    MERGE 2

--- RFScraper/DBController.py
+++ RFScraper/DBController.py
@@ -12,11 +12,11 @@ class DBController:
 
     def __init__(self):
         self.conn = sqlite3.connect('miuDB.db')
-        self.c = conn.cursor()
+        self.c = self.conn.cursor()
         # create tables here???
         self.c.execute("""CREATE TABLE IF NOT EXISTS Users (email varchar(30) primary key, words varchar(30) )""")
-		self.c.execute("""CREATE TABLE IF NOT EXISTS Keywords (words varchar(30) primary key )""")
-		self.c.execute("""CREATE TABLE IF NOT EXISTS RFPosts ( id INTEGER PRIMARY KEY AUTOINCREMENT, title varchar(30), url varchar(30) UNIQUE) """)	
+	    self.c.execute("""CREATE TABLE IF NOT EXISTS Keywords (words varchar(30) primary key )""")
+		self.c.execute("""CREATE TABLE IF NOT EXISTS RFPosts ( id INTEGER PRIMARY KEY AUTOINCREMENT, title varchar(30), url varchar(30) UNIQUE) """)
 		self.c.execute("""CREATE TABLE IF NOT EXISTS RFUsers (email varchar(30) , words varchar(30), primary key(email, words), FOREIGN KEY(email) REFERENCES Users(email), FOREIGN KEY(words) REFERENCES RFPosts(title)) """)
 		self.conn.commit()
 		self.c.close()
@@ -24,7 +24,7 @@ class DBController:
         #pass
         #initialize cursors here. Makes sure there's only one controller interacting with database
 
-    def getkeywords(self): #TODO
+    def getkeywords(self):
         """
         fetches DB for keywords and corresponding emails.
 
@@ -34,7 +34,7 @@ class DBController:
         IMPLEMENTED BY USMAN
         """
         c1 = self.conn.cursor()
-		result1 = c1.execute('SELECT * FROM Users')	
+		result1 = c1.execute('SELECT * FROM Users')
 		dict_words = {}
 		for r in result1.fetchall():
 			c2 = self.conn.cursor()
